6e776206b34d652493ebc1f9f2ecc04645266500
Amit Awate <119167734+amitawate123@users.noreply.github.com>
2024-12-28 00:21:42 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,96 @@
+
+// Variables
+
+// let a = 2
+// let b = 2
+// console.log(a + b)
+
+// let currentResult = 5;
+
+// let newResult = currentResult + 10;
+
+// console.log(newResult)
+
+// Constants
+
+// let a = 2
+// let b = 2
+// console.log(a + b)
+// console.log(a - b)
+// console.log(a * b)
+// console.log(a / b)
+// console.log(a % b)
+
+// let greet = "Hello Everyone";
+
+// console.log(greet)
+
+
+// variables and datatypes
+
+// function example(){
+//     let x = 10;
+//     console.log(x)
+// }
+
+// example()
+// console.log(x)
+
+
+// {
+//     let a = 10;
+//     console.log(a)
+// }
+
+// let x = 10;
+// console.log(x);
+// x = 'hello'
+// console.log(x)
+
+
+// DATATYPES IN JAVASCRIPT
+
+// let name = "Hello"
+// console.log(typeof(name))
+
+// let marks = true;
+// console.log(typeof(marks))
+
+// let marks = 10;
+// console.log(typeof(marks))
+
+//OPERATORS IN JAVASCRIPT
+
+// let a = 10;
+// let b = 5;
+// console.log(a+b)
+
+// let age = 17;
+// let status = (age>18)?'i can vote' : 'i cannot vote'
+// console.log(status)
+
+// console.log(10 >>1)
+
+// let age = 19;
+// if(age > 18){
+//     console.log("Vote")
+// }
+// else {
+// console.log("cannot vote")
+// }
+
+let num = 4;
+
+switch(num) {
+    case 1: console.log("A");
+    break;
+    case 2: console.log("B");
+    break;
+    case 3: console.log("C");
+    break;
+    case 4: console.log("D");
+    break;
+    default: console.log("F");
+}
+
+
