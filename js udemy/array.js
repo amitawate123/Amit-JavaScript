73ff92158516c691f820c4af0f3776e16f0c3eea
Amit Awate <119167734+amitawate123@users.noreply.github.com>
2024-12-30 09:23:52 +0530

    Add files via upload

---
+++ js udemy/array.js
@@ -0,0 +1,41 @@
+// let obj = {
+//     name : "Hello",
+//     age : 22,
+//     weight : 50
+// }
+
+// console.log(obj)
+
+
+// ARRAY IN JAVASCRIPT
+
+// let arr = [1, 2, 3, 4, 5];
+// console.log(arr)
+// console.log(typeof(arr))
+
+// let arr = ["Name", "Hello", "How", "Good"];
+// console.log(arr[2]);
+// console.log(arr[3]);
+
+
+// let arr = ["Hello", "Name", "Good", "True", 45];
+
+// arr.pop('Hi');
+// arr.unshift();
+// arr.push(20); 
+// arr.push(30)
+// arr.slice(2,5)
+// console.log(arr)
+
+// let arr = [10, 20, 30]
+
+// let ansArray = arr.map((number) =>{
+//     return number*number;
+// })
+// console.log(ansArray)
+
+// let arr = [10, 20, 30, 40, 50]
+// // console.log(arr.indexOf(3));
+
+// let length = arr.length;
+// console.log(length)
