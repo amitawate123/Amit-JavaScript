3b80301594d92674e1bbccc5a3b3d1e22983802f
Amit Awate <119167734+amitawate123@users.noreply.github.com>
2024-12-30 11:58:12 +0530

    Add files via upload

---
+++ js udemy/hoisting.js
@@ -0,0 +1,48 @@
+
+
+
+// sayHello()
+// function sayHello(){
+//     console.log("Hello")
+// }
+
+
+
+// myName("Hello")
+
+// function myName(myName){
+//     console.log(myName)
+// }
+
+// foo()
+// function foo(){
+//     console.log("Hello WOrld")
+// }
+
+
+// {
+// console.log(age);
+
+// var age = 23
+// }
+
+// console.log(x)
+// var x = 5;
+
+
+// FUNCTION CALL STACK IN JAVASCRIPT
+
+
+// function greetme(greet,fullname){
+//         console.log("Hello", fullname)
+//         greet();
+// }
+
+// function greet(){
+//     console.log("Greeting for the day")
+// }
+
+
+
+
+// greetme(greet ,"Earth")
