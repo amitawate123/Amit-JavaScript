b0566e90bb7070c16268efa2bfa6f7ac6eb4df0a
Amit Awate <119167734+amitawate123@users.noreply.github.com>
2024-12-30 11:59:17 +0530

    Add files via upload

---
+++ js udemy/var_scope.js
@@ -0,0 +1,86 @@
+// VARIABLE SCOPING IN JAVASCRIPT
+
+
+// GLOBAL SCOPE
+
+
+// var age = 20;
+// console.log(age)
+
+// {
+//     console.log(age)
+// }
+
+// if(true){
+//     console.log(age)
+// }
+
+// for(let i =0; i<2; i++){
+//     console.log(age)
+// }
+
+// { 
+//     var x = 10;
+// }
+
+// console.log(x)
+
+// let carName = "Volvo";
+
+// function myname(){
+//     console.log(carName)
+// }
+
+// myname()
+
+// var x = 1;
+// let y = 2;
+// const z = 3;
+
+// function getno(){
+//     console.log(x);
+//     console.log(y);
+//     console.log(z)
+// }
+
+// getno()
+
+
+// FUNCTION SCOPE 
+
+// function sayHello(){
+//     var name = "Earth"
+//     console.log("Hello", name)
+// }
+
+// sayHello()
+
+
+// function myname(){
+//     for(let i=0; i<5; i++){
+//         console.log("Hello")
+//     }
+// }
+
+// myname();
+
+
+// BLOCK SCOPE
+
+// {
+//     var x = 20;
+// }
+
+// console.log(x) 
+
+// TEMPORAL DEAD ZONE
+
+// function example(){
+    
+//     let x = 10;
+//     console.log(x)
+// }
+
+// example();
+
+
