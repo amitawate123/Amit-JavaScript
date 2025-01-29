f7da13ddc6125124f5992d05c27cf8a99fd5121a
Amit Awate <119167734+amitawate123@users.noreply.github.com>
2025-01-28 22:08:33 -0800

    Add files via upload

---
+++ js udemy/math.js
@@ -0,0 +1,17 @@
+// console.log(Math.PI);
+
+// console.log(Math.max(10,20,30,40,50,60));
+
+// console.log(Math.min(10,20,30,40,50,60));
+
+// console.log(Math.round(1.4));
+
+// console.log(Math.floor(1.7));
+
+// console.log(Math.ceil(1.1));
+
+// console.log(Math.abs(-5));
+
+// console.log(Math.random());
+
+// console.log(Math.sqrt(4));
