b175c8855076229d641da00a8747a8df7cf1f594
Amit Awate <119167734+amitawate123@users.noreply.github.com>
2025-01-28 22:09:31 -0800

    Add files via upload

---
+++ js udemy/obj_clone.js
@@ -0,0 +1,58 @@
+// SPREAD OPERATOR
+
+// let obj = {
+//    age: 12,
+//    wt: 50,
+//    ht: 180
+// };
+// console.log(obj)
+
+// obj.color = "White";
+
+// console.log(obj)
+
+// let clone = {...obj}
+// console.log(clone)
+
+// let src = {
+//    age: 12,
+//    wt: 50,
+//    ht: 180
+// };
+
+// src.age = 90
+// src.wt = 60
+
+// let dest = {...src};
+// console.log(src)
+// console.log(dest)
+
+// let org = { a:1, b:2, c:3, d:4};
+// let dest = {...org}
+
+// org.a = 5
+// console.log(org)
+// console.log(dest)
+
+
+// Object.assign()
+
+// let org = { a:1, b:2, c:3, d:4};
+// let dest = Object.assign({}, org)
+// console.log(org)
+// console.log(dest)
+
+// let src = {
+//     age:12,
+//     wt:68,
+//     ht:180
+// }
+
+
+// src.age = 20;
+// let dest = Object.assign({}, src)
+
+
+// console.log(src)
+// console.log(dest)
+
