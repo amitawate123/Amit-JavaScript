8a5d65ef7ec7d7057b7ef8fd8675a07f93f18df9
Amit Awate <119167734+amitawate123@users.noreply.github.com>
2025-01-28 22:07:15 -0800

    Add files via upload

---
+++ js udemy/classes.js
@@ -0,0 +1,37 @@
+// CLASSESIN JAVASCRIPT
+
+// class Human{
+//     age = 20;  // public 
+//     #wt = 50;  // private 
+//     ht = 180;
+
+//     walking(){
+//         console.log("I am walking", this.#wt)
+//     }
+
+//     running(){
+//         console.log("I am running")
+//     }
+// }
+
+// let obj = new Human();
+// console.log(obj.age);
+
+
+// obj.walking();
+
+// function name(fname, lname= fname.toUpperCase()){
+//     console.log("My name is", fname," ", lname)
+// }
+// name("hello",)
+
+// function solve(value = {age: 12, ht:180, wt:50}){
+//     console.log("Hello", value)
+// }
+// solve()
+
+// function solve(value = ["Hello", "Good", "How"]){
+//     console.log("Hello", value)
+// }
+// solve()
+
